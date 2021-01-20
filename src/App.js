fde97efb5c7baa29cfb2f3791358e30e3d04cfc6
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-01-20 16:04:50 +0300

    added products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import products from "./products";
 import main from "./homepagephoto.jpeg";
 
 function App() {
+  const productsList = products.map((product) => (
+    <div className="column">
+      <img src={product.image} alt={product.name} className="productPhoto" />
+      <p className="App"> {product.name}</p>
+      <p className="App"> {product.price}</p>
+    </div>
+  ));
+
   return (
     <div>
       <h1 className="App">Smart Hub</h1>
       <p className="App"> Make your home smarter</p>
       <img src={main} alt="main" className="photo" />
+      <br />
+      <h2 className="App">Products</h2>
+      {productsList}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "Echo Dot",
+    price: "20 BHD",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/612KpcXcFBL._AC_SL1000_.jpg",
+  },
+  {
+    name: "Philips LED Smart Bulb",
+    price: "6 BHD",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81Wp5ldN2TL._AC_SL1500_.jpg",
+  },
+];
+
+export default products;
