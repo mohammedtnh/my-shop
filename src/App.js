342430cc3419bab9a68baa71b869da5f068217ab
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-01-20 13:30:52 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import main from "./homepagephoto.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="App">Smart Hub</h1>
+      <p className="App"> Make your home smarter</p>
+      <img src={main} alt="main" className="photo" />
     </div>
   );
 }
